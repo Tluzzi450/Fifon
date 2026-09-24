d4316edd93c9fdea69d863a1708c3213c1df8447
Tluzzi450 <tluzzi450@alumnos.frh.utn.edu.ar>
2026-09-23 21:30:29 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,16 @@
-   window.PF_FIREBASE = {
-     serie: "principal",
-     jugadores: {
-       'nahuellopepe2903@gmail.com': "Cabe",
-       'tluzzi450@alumnos.frh.utn.edu.ar': "Verga"
-     },
-   config: {
-  apiKey: "AIzaSyCEpmDe0hhhWSwxK9JAcYMMtLOVCCBpvtY",
-  authDomain: "app-fifa-8ed3a.firebaseapp.com",
-  projectId: "app-fifa-8ed3a",
-  storageBucket: "app-fifa-8ed3a.firebasestorage.app",
-  messagingSenderId: "981585234295",
-  appId: "1:981585234295:web:5298498765c46dfca2f972",
-  measurementId: "G-X0692DKQW1"
+window.PF_FIREBASE = {
+  serie: "principal",
+  jugadores: {
+    "nahuellopepe2903@gmail.com": "Cabe",
+    "tluzzi450@alumnos.frh.utn.edu.ar": "Verga"
+  },
+  config: {
+    apiKey: "AIzaSyCEpmDe0hhhWSwxK9JAcYMMtLOVCCBpvtY",
+    authDomain: "app-fifa-8ed3a.firebaseapp.com",
+    projectId: "app-fifa-8ed3a",
+    storageBucket: "app-fifa-8ed3a.firebasestorage.app",
+    messagingSenderId: "981585234295",
+    appId: "1:981585234295:web:5298498765c46dfca2f972",
+    measurementId: "G-X0692DKQW1"
+  }
 };
